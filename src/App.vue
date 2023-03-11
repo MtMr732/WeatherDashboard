0942eade87f8c47b0a3e41a55e98014773d430b3
toru_motomura\tbenc <tbencun156@gmail.com>
2023-03-11 16:30:06 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { inject } from 'vue'
+// この中に、template内で利用する変数や関数を定義しておく。
+import InputForm from './components/InputForm.vue'
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <InputForm />
+    <CurrentWeather />
+    <HourlyForecast />
+    <DailyForecast />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+
+// const app =
+createApp(App).mount('#app')
+// app.use(VueAixous,axios);
